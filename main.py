3f5e637e141589d88635e376e7239c05dd0ed637
lluneko <117767742+lluneko@users.noreply.github.com>
2023-03-16 17:02:54 +0300

    o

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Привет, Яндекс!"
+    return render_template('base.html', title='Заготовка')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=5000, host='127.0.0.1')

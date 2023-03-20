a57dcd5ba2a9e4820b34206f4ddff4169ca44604
NightCAT213 <busyamouse@yandex.ru>
2023-03-20 16:56:00 +0300

    ddd

--- parsing_sites/sts.py
+++ parsing_sites/sts.py
@@ -1,6 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-res = requests.get('https://kanobu.ru/games/popular/')
+# пример запроса меток
+res = requests.get('https://kanobu.ru/games/popular')
 soup = BeautifulSoup(res.text, 'lxml')
-print(soup.select_one('[class^="BaseElementCard_body__fcrUh"]'))
+a = str(soup.find_all('div', class_='knb-cell'))
+games = []  # метки игры на сайте
+a = a.split('href="')
+a.pop(0)
+for i in a:
+    i = i[7:-1]
+    index = i.find("/")
+    games.append(i[0:index])
+games = list(set(games))
+print(games)
+

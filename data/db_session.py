05331844b9aaee1fbcc6e04ce2013d5b163deca0
lluneko <117767742+lluneko@users.noreply.github.com>
2023-04-10 18:14:08 +0300

    ok

--- data/db_session.py
+++ data/db_session.py
@@ -1,33 +1,29 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session
-import sqlalchemy.ext.declarative as dec
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-SqlAlchemyBase = dec.declarative_base()
+db_path = 'users_base.sqlite'
+engine = create_engine('sqlite:///%s' % db_path, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 
-__factory = None
-
-
-def global_init(db_file):
-    global __factory
-
-    if __factory:
-        return
-
-    if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
-
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    print(f"Подключение к базе данных по адресу {conn_str}")
-
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine)
-
-    from . import __all_models
-
-    SqlAlchemyBase.metadata.create_all(engine)
+id = 0
 
 
-def create_session() -> Session:
-    global __factory
-    return __factory()
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    password = Column(String)
+
+
+def add_user(nameus, passwordus):
+    global id, session
+    id += 1
+    c = Users(name=nameus, password=passwordus)
+    session.add(c)
+    session.commit()
+

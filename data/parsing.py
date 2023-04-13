9baee84c3b5842b7863aa2c3cfa6b89ac49db873
lluneko <117767742+lluneko@users.noreply.github.com>
2023-04-13 16:12:02 +0300

    youuuu

--- data/parsing.py
+++ data/parsing.py
@@ -3,6 +3,15 @@ import requests
 
 res = requests.get('https://skazbuka.com/')
 soup = BeautifulSoup(res.text, 'lxml')
-a = str(soup.find_all('div', class_="t480__descr t-descr t-descr_md", limit=3))
+a = str(soup.find_all('div', class_="t480__descr t-descr t-descr_md", limit=3)).split('<div class="t480__descr '
+                                                                                      't-descr t-descr_md" field="'
+                                                                                      'descr" style="">')
+a = a[1:]
+for i in range(len(a)):
+    if a[i] != a[-1]:
+        a[i] = a[i][:-8]
+    else:
+        a[i] = a[i][:-7]
+a = ' '.join(' '.join(a).split('<br/>'))
 print(a)
 
--- parsing_sites/connection.py
+++ parsing_sites/connection.py
@@ -111,7 +111,18 @@ def pars(title):
             'наконец, социализации. Родители могут выбрать как один курс, так и все сразу. Система не перегружает ' \
             'ребенка, даже если он занимается всеми предложенными активностями.'
     if title == 'Сказбука':
-        pass
+        res = requests.get('https://skazbuka.com/')
+        soup = BeautifulSoup(res.text, 'lxml')
+        a = str(soup.find_all('div', class_="t480__descr t-descr t-descr_md", limit=3)).split('<div class="t480__descr '
+                                                                                              't-descr t-descr_md" field="'
+                                                                                              'descr" style="">')
+        a = a[1:]
+        for i in range(len(a)):
+            if a[i] != a[-1]:
+                a[i] = a[i][:-8]
+            else:
+                a[i] = a[i][:-7]
+        a = ' '.join(' '.join(a).split('<br/>'))
     return a
 
 
